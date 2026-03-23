90f57781292505c22b59a808ba1458e2c79d240a
poornendhupkumar-afk <poornendhupkumar@gmail.com>
2026-03-24 00:28:20 +0530

    Tested analyzer and verified output

---
+++ test_analyzer.py
@@ -0,0 +1,14 @@
+from analyzer import analyze   # make sure filename is analyzer.py
+
+result = analyze(".")
+
+print("FILES:")
+print(result["files"])
+
+print("\nFUNCTIONS:")
+for k, v in result["functions"].items():
+    print(k, ":", v)
+
+print("\nIMPORTS:")
+for k, v in result["imports"].items():
+    print(k, ":", v)
